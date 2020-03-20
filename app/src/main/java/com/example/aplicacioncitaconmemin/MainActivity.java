b9addffebafe6679b8e90e640c8964898a76c452
Juan Pablo Velazco <juanpa.jpvv@gmail.com>
2020-03-20 15:30:13 -0600

    Login Mamalon

--- app/src/main/java/com/example/aplicacioncitaconmemin/MainActivity.java
+++ app/src/main/java/com/example/aplicacioncitaconmemin/MainActivity.java
@@ -8,11 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText username, pass;
     Button login;
+    private UserDB userDB;
 
 
     @Override
@@ -21,14 +23,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         username = findViewById(R.id.username);
         pass = findViewById(R.id.pass);
-
+        userDB = new UserDB(this);
 
 
     }
     public void abrirRegistro(View v){
         Intent intentito = new Intent(this, RegisterActivity.class);
-
         startActivity(intentito);
+    }
+
+    public void login(View v){
+        boolean intento = userDB.tryLogin(username.getText() + "", pass.getText() + "");
+        if (intento){
+            Toast.makeText(this, "Iniciado de sesión correcto", Toast.LENGTH_SHORT).show();
+        } else{
+            Toast.makeText(this, "Iniciado de sesión incorrecto", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
--- app/src/main/java/com/example/aplicacioncitaconmemin/RegisterActivity.java
+++ app/src/main/java/com/example/aplicacioncitaconmemin/RegisterActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText username, pass;
+    private UserDB userDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,21 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         username = findViewById(R.id.username);
         pass = findViewById(R.id.pass);
+        userDB = new UserDB(this);
+    }
 
+    public void register(View v){
+        boolean register = userDB.registerUser(username.getText() + "", pass.getText() + "", "555555555");
+        if (register){
+            Toast.makeText(this, "Registro completado", Toast.LENGTH_SHORT).show();
+        } else{
+            Toast.makeText(this, "Ese usuario ya ha sido escogido, selecciona otro porfavor", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void resetDB(View v){
+        userDB.resetDB();
     }
 
     public void cerrarRegistro(View v){
