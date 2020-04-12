9c519281475daa198d9a381b54d8cf3d978e75df
Juan Pablo Velazco Velasquez <juanpa.jpvv@gmail.com>
2020-04-12 18:31:55 -0500

    ora si interfaz crud chida

--- app/src/main/java/com/example/aplicacioncitaconmemin/FragmentHome.java
+++ app/src/main/java/com/example/aplicacioncitaconmemin/FragmentHome.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 public class FragmentHome extends Fragment {
+
+    private EditText title, description, url;
+    private Button create, delete, update;
 
     String[] imageUrls = new String[]{
             "https://picsum.photos/504/600",
@@ -42,6 +48,15 @@ public class FragmentHome extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home,container,false);
+
+        title = v.findViewById(R.id.title);
+        description = v.findViewById(R.id.description);
+        url = v.findViewById(R.id.url);
+
+        create = v.findViewById(R.id.create);
+        update = v.findViewById(R.id.update);
+        delete = v.findViewById(R.id.delete);
+
 
         adapter = new Adapter(getActivity(),imageUrls,titles,descriptions);
 
